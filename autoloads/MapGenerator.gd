1c357f28a5da240a69fab0ace378a2e5a17643ad
Sean Maltby <seanmaltby@gmail.com>
2025-01-26 12:45:35 -0600

    Update map gen

--- autoloads/MapGenerator.gd
+++ autoloads/MapGenerator.gd
@@ -7,19 +7,20 @@ const TileMaterial = {
 	&"STONE": 1,
 	&"COAL": 2,
 	&"IRON": 3,
+	&"OBSIDIAN": 4,
 }
 
 
 # The lower the number, the bigger the features
 @export var initial_scale = 0.081
 # High number for ore so we get lots of little pockets (big number = small features)
-@export var ore_scale = 0.281
+@export var ore_scale = 0.481
 
 # Similar to conway's game of life, but with different parameters
 # Populated tiles with fewer neighbours than the death_limit will die
 # Empty tiles with more neighbours than the birth_limit will spawn in
 # Serves to smooth out the caves after generating them from the height map
-@export var simulation_steps = 6
+@export var simulation_steps = 4
 @export var death_limit = 3
 @export var birth_limit = 6
 
@@ -42,15 +43,16 @@ func generate(_width: int, _height: int) -> Array:
 		return map
 	generating = true
 
-	noise.noise_type = FastNoiseLite.NoiseType.TYPE_SIMPLEX
-	noise.fractal_type = FastNoiseLite.FractalType.FRACTAL_FBM
-	noise.frequency = 0.5
+	noise.noise_type = FastNoiseLite.NoiseType.TYPE_PERLIN
+	noise.fractal_type = FastNoiseLite.FractalType.FRACTAL_NONE
+	noise.frequency = 1
 	noise.seed = randi()
 
-	noise_with_octaves.noise_type = FastNoiseLite.NoiseType.TYPE_SIMPLEX
-	noise_with_octaves.fractal_type = FastNoiseLite.FractalType.FRACTAL_FBM
+	noise_with_octaves.noise_type = FastNoiseLite.NoiseType.TYPE_PERLIN
+	noise_with_octaves.fractal_type = FastNoiseLite.FractalType.FRACTAL_FBM   
 	noise_with_octaves.fractal_octaves = 8
-	noise_with_octaves.frequency = 0.5
+	noise_with_octaves.fractal_gain = 0.45
+	noise_with_octaves.frequency = 1
 	noise_with_octaves.seed = randi()
 
 	width = _width
@@ -62,6 +64,7 @@ func generate(_width: int, _height: int) -> Array:
 
 	gen_height_map()
 	map = smooth(map, width, height, simulation_steps, death_limit, birth_limit)
+	findOpenSpaces()
 	addPrefabs()
 	placeOre(TileMaterial.COAL)
 	placeOre(TileMaterial.IRON)
@@ -76,8 +79,8 @@ func placeOre(oreType: int):
 			var index = getIndex(x, y)
 			if map[index] != TileMaterial.STONE:
 				continue
-			var xNoise = x * 0.281
-			var yNoise = y * 0.281
+			var xNoise = x * ore_scale
+			var yNoise = y * ore_scale
 			var result = get_noise(xNoise, yNoise, oreType * 99.9)
 			if result > 0.75:
 				map[index] = oreType
@@ -86,8 +89,12 @@ func addPrefabs():
 	addHomebase()
 
 func addHomebase():
-	for i in range(-5, 6):
-		for j in range(-5, 6):
+	for i in range(-12, 13):
+		for j in range(-12, 13):
+			if abs(i) > 5 && abs(j) > 1:
+				continue
+			if abs(j) > 5 && abs(i) > 1:
+				continue
 			var x = int(center.x) + i
 			var y = int(center.y) + j
 			var index = getIndex(x, y)
@@ -101,104 +108,40 @@ func gen_height_map():
 			var yNoise = y * initial_scale
 			var dstFromCenter = distanceFromCenter(x, y)
 
-			var result = get_noise(xNoise, yNoise, 0.5)
+			var result = get_noise_with_octaves(xNoise, yNoise, 0.5)
 
-			var threshold = cosInterpolate(dstFromCenter / (maxDstFromCenter * 0.9), 0.035, 0.000)
+			# Store off values very close to 0.5, as these are likely the center of larger caves
+			var threshold = cosInterpolate(dstFromCenter / (maxDstFromCenter * 0.9), 0.033, 0.000)
 			if threshold < 0:
 				threshold = 0
 			# Spawn stone when not within the threshold. Makes tunnels.
 			# Threshold decreases farther from center, decreasing the number of tunnels.
-			if result < 0.5 - threshold || result > 0.5 + threshold:
+			if (result < 0.5 - threshold || result > 0.5 + threshold) && result < 0.67:
 				map[index] = TileMaterial.STONE
 
-# func smooth():
-#     for step in range(simulation_steps):
-#         smoothStep()
-		
+func findOpenSpaces():
+	for y in range(height):
+		var y1 = y * width
+		for x in range(width):
+			var index = x + y1
+			if map[index] == TileMaterial.EMPTY:
+				var neighbours = countNeighboursFar(x, y, 2)
+				if neighbours <= 2:
+					map[index] = TileMaterial.OBSIDIAN
 
-# func smoothStep() -> Array:
-#     for y in range(height):
-#         var y1 = y * width
-#         for x in range(width):
-#             var index = x + y1
-#             var neighbours = countNeighbours(x, y)
-#             if map[index]:
-#                 # Lonely cells kill themselves
-#                 if neighbours < death_limit:
-#                     temp_map[index] = TileMaterial.EMPTY
-#                 else:
-#                     temp_map[index] = TileMaterial.STONE
-#             else:
-#                 # Dead cells revive when surrounded
-#                 if neighbours > birth_limit:
-#                     temp_map[index] = TileMaterial.STONE
-#                 else:
-#                     temp_map[index] = TileMaterial.EMPTY
-#     var temp = map
-#     map = temp_map
-#     temp_map = temp
-#     return map
-			
-
-func countNeighbours(x: int, y: int) -> int:
+func countNeighboursFar(x: int, y: int, dst: int) -> int:
 	var count = 0
-	var neighbourX = x + -1
-	var neighbourY = y + -1
-	if neighbourX < 0 || neighbourY < 0 || neighbourX >= width || neighbourY >= height:
-		count += 1 # Off the map, assume alive to avoid clearing out the edges of the map
-	elif map[getIndex(neighbourX, neighbourY)]:
-		count += 1
-	
-	neighbourX = x + -1
-	neighbourY = y + 0
-	if neighbourX < 0 || neighbourY < 0 || neighbourX >= width || neighbourY >= height:
-		count += 1 # Off the map, assume alive to avoid clearing out the edges of the map
-	elif map[getIndex(neighbourX, neighbourY)]:
-		count += 1
-	
-	neighbourX = x + -1
-	neighbourY = y + 1
-	if neighbourX < 0 || neighbourY < 0 || neighbourX >= width || neighbourY >= height:
-		count += 1 # Off the map, assume alive to avoid clearing out the edges of the map
-	elif map[getIndex(neighbourX, neighbourY)]:
-		count += 1
-	
-	neighbourX = x + 0
-	neighbourY = y + -1
-	if neighbourX < 0 || neighbourY < 0 || neighbourX >= width || neighbourY >= height:
-		count += 1 # Off the map, assume alive to avoid clearing out the edges of the map
-	elif map[getIndex(neighbourX, neighbourY)]:
-		count += 1
-	
-	neighbourX = x + 0
-	neighbourY = y + 1
-	if neighbourX < 0 || neighbourY < 0 || neighbourX >= width || neighbourY >= height:
-		count += 1 # Off the map, assume alive to avoid clearing out the edges of the map
-	elif map[getIndex(neighbourX, neighbourY)]:
-		count += 1
-	
-	neighbourX = x + 1
-	neighbourY = y + -1
-	if neighbourX < 0 || neighbourY < 0 || neighbourX >= width || neighbourY >= height:
-		count += 1 # Off the map, assume alive to avoid clearing out the edges of the map
-	elif map[getIndex(neighbourX, neighbourY)]:
-		count += 1
-	
-	neighbourX = x + 1
-	neighbourY = y + 0
-	if neighbourX < 0 || neighbourY < 0 || neighbourX >= width || neighbourY >= height:
-		count += 1 # Off the map, assume alive to avoid clearing out the edges of the map
-	elif map[getIndex(neighbourX, neighbourY)]:
-		count += 1
-	
-	neighbourX = x + 1
-	neighbourY = y + 1
-	if neighbourX < 0 || neighbourY < 0 || neighbourX >= width || neighbourY >= height:
-		count += 1 # Off the map, assume alive to avoid clearing out the edges of the map
-	elif map[getIndex(neighbourX, neighbourY)]:
-		count += 1
+	for i in range(-dst, dst+1):
+		for j in range(-dst, dst+1):
+			if i == 0 && j == 0:
+				continue
+			var neighbourX = x + i
+			var neighbourY = y + j
+			if neighbourX < 0 || neighbourY < 0 || neighbourX >= width || neighbourY >= height:
+				count += 1 # Off the map, assume alive to avoid clearing out the edges of the map
+			elif map[getIndex(neighbourX, neighbourY)]:
+				count += 1
 	return count
-
 
 func indexToX(index: int) -> int:
 	return index % width
--- scripts/mapgen/debug_map_drawer.gd
+++ scripts/mapgen/debug_map_drawer.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 const TileMaterial = MapGenerator.TileMaterial
 
-const WIDTH = 200
-const HEIGHT = 200
+const WIDTH = 333
+const HEIGHT = 333
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,11 +23,13 @@ func _draw():
 			TileMaterial.STONE:
 				color = Color.DARK_GRAY
 			TileMaterial.COAL:
-				color = Color.BLACK
+				color = Color.DARK_GRAY
 			TileMaterial.IRON:
 				color = Color.GOLD
+			TileMaterial.OBSIDIAN:
+				color = Color.PURPLE
 			_:
 				color = Color.WHITE
 
-		draw_rect(Rect2(x * 5, y * 5, 5.0, 5.0), color)
+		draw_rect(Rect2(x * 2, y * 2, 2.0, 2.0), color)
 	pass
