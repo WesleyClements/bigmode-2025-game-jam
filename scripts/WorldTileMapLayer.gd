b549276d12cbaa994cac32433350241fa263c834
WesleyClements <wesley.clements@gmail.com>
2025-01-26 15:50:40 -0500

    made destroying tiles drop resources

--- scripts/WorldTileMapLayer.gd
+++ scripts/WorldTileMapLayer.gd
@@ -1,5 +1,8 @@
 extends TileMapLayer
 
+@export var item_registry: ItemRegistry
+@export var wall_tileset_atlas_id := 0
+
 func _ready() -> void:
 	MessageBuss.request_grid_cell_clear.connect(on_grid_cell_clear_request)
 
@@ -16,4 +19,22 @@ func on_grid_cell_clear_request(cell_pos: Vector2i) -> void:
 	if cell_data == null:
 		return
 	MessageBuss.grid_cell_clearing.emit(cell_pos)
+	var item_drops: Dictionary = cell_data.get_custom_data(&"item_drops")
+	if not item_drops == null:
+		for item_name: StringName in item_drops.keys():
+			var drop_config: Dictionary = item_drops[item_name]
+			assert(drop_config != null)
+			assert(drop_config.min != null)
+			assert(drop_config.max != null)
+			var item_type: ItemRegistry.ItemType = ItemRegistry.ItemType[item_name]
+			var drop_amount := randi_range(drop_config.min, drop_config.max)
+			var scene_template := item_registry.get_entity_scene(item_type)
+			if scene_template == null: # TODO assert not null
+				continue
+			for _i in range(drop_amount):
+				var scene: Node2D = scene_template.instantiate()
+				add_child(scene)
+				scene.global_position = to_global(map_to_local(cell_pos)) + Vector2(randf() - 0.5, randf() - 0.5) * 2.0 * 4.0 # TODO no magic numbers
+
+
 	erase_cell.call_deferred(cell_pos)
