7e9c5f38f04c2ab7592adf56ad7b3f6b787a278b
WesleyClements <wesley.clements@gmail.com>
2025-02-01 09:43:31 -0500

    made interacting with the generator deposit an amount initially then deposit more if held

--- scenes/machines/gerbil_generator_interaction.gd
+++ scenes/machines/gerbil_generator_interaction.gd
@@ -3,16 +3,38 @@ extends Node
 const Player = preload("res://scenes/player.gd")
 const GerbilGenerator = preload("res://scenes/machines/gerbil_generator.gd")
 
+@export var initial_deposit_amount: float = 1.0
 @export var deposit_rate: float = 12.0
 
+var frames_since_interaction := 0
+var continuously_depositing := false
+
 @onready var generator: GerbilGenerator = owner
+@onready var continuous_interaction_timer: Timer = $ContinuousInteractionTimer
+
+func _physics_process(_delta: float) -> void:
+	frames_since_interaction += 1
+	if frames_since_interaction > 1:
+		continuously_depositing = false
 
 func interact(node: Node, _just_pressed: bool) -> void:
 	assert(node is Player)
+	frames_since_interaction = 0
 	var player := node as Player
 	if is_zero_approx(player.coal_count):
 		player.coal_count = 0.0
 		return
-	var transfer_amount := minf(player.coal_count, deposit_rate * get_physics_process_delta_time())
-	player.coal_count -= transfer_amount
-	generator.fuel += transfer_amount
+	if _just_pressed:
+		var transfer_amount := minf(player.coal_count, initial_deposit_amount)
+		player.coal_count -= transfer_amount
+		generator.fuel += transfer_amount
+		continuous_interaction_timer.start()
+	elif continuously_depositing:
+		var transfer_amount := minf(player.coal_count, deposit_rate * get_physics_process_delta_time())
+		player.coal_count -= transfer_amount
+		generator.fuel += transfer_amount
+
+func on_continuous_interaction_timer_timeout() -> void:
+	if frames_since_interaction > 1:
+		return
+	continuously_depositing = true
