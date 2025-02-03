74e4334281c682aed107b5ee7cc69ea5e503c394
Sean Maltby <seanmaltby@gmail.com>
2025-02-02 21:57:00 -0600

    Fix victory time

--- scenes/ui/you_win_scene.gd
+++ scenes/ui/you_win_scene.gd
@@ -3,5 +3,5 @@ extends "res://scripts/ui_wrapper.gd"
 @onready var time_taken_label: Label = $VBoxContainer/TimeTakenLabel
 
 func _ready() -> void:
-	var time_since_start: float = 2.5 if self == get_tree().current_scene else VictoryHandler.get_time_since_start()
+	var time_since_start: float = VictoryHandler.get_time_since_start()
 	time_taken_label.text = "Time taken %d:%06.3f" % [floorf(time_since_start / 60.0), fmod(time_since_start, 60.0)]
