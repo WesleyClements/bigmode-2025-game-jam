4dc05185b2a578e3910bbb3e42b6c634f0df9f03
WesleyClements <wesley.clements@gmail.com>
2025-02-02 21:59:25 -0500

    fixed my horrendous quick fix for portal build progress propagation

--- scenes/iron_display_panel.gd
+++ scenes/iron_display_panel.gd
@@ -8,6 +8,6 @@ func _ready() -> void:
 	position.y = positions[0].y
 	position.x = -size.x / 2.0
 
-func on_iron_changed(iron: float) -> void:
-	position.y = positions[floor((positions.size() - 1) * iron / 100.0)].y # TODO: no magic numbers
+func on_build_progress_changed(value: float) -> void:
+	position.y = positions[floor((positions.size() - 1) * value)].y
 	position.x = -size.x / 2.0
--- scenes/the_portal.gd
+++ scenes/the_portal.gd
@@ -4,6 +4,7 @@ const WorldTileMapLayer = preload("res://scripts/WorldTileMapLayer.gd")
 const TileMapDetectionArea = preload("res://scenes/tile_map_detection_area.gd")
 
 signal iron_changed(value: float)
+signal build_progress_changed(value: float)
 
 signal hover_entered()
 signal hover_exited()
@@ -22,12 +23,11 @@ var iron: float = 0.0:
 		iron = value
 		if is_equal_approx(iron, target_amount):
 			iron = target_amount
-		var frame_count := portal_sprite.sprite_frames.get_frame_count(&"default")
-		portal_sprite.frame = floorf((frame_count - 1) * iron / target_amount) as int
+			build_progress_changed.emit(1.0)
+		else:
+			build_progress_changed.emit(iron / target_amount)
 		iron_changed.emit(iron)
-		if iron == target_amount: # so many magic numbers
-			VictoryHandler.you_win.emit()
-			portal_sprite.frame = frame_count - 1
+
 
 @onready var world_map: WorldTileMapLayer = get_parent()
 @onready var iron_display: Label = $IronDisplayPanel/IronDisplay
@@ -41,7 +41,7 @@ func _ready() -> void:
 	hover_entered.connect(on_hover_changed.bind(true))
 	hover_exited.connect(on_hover_changed.bind(false))
 
-func on_energy_changed() -> void:
+func on_iron_changed() -> void:
 	iron_display.text = "%s / %s" % [ceilf(iron), target_amount]
 
 func on_player_interaction_area(_body: Node, entered: bool) -> void:
@@ -49,3 +49,11 @@ func on_player_interaction_area(_body: Node, entered: bool) -> void:
 
 func on_hover_changed(_is_hovered: bool) -> void:
 	pass
+
+func on_build_progress_changed(value: float) -> void:
+	var frame_count := portal_sprite.sprite_frames.get_frame_count(&"default")
+	if not is_equal_approx(value, 1.0):
+		portal_sprite.frame = floorf((frame_count - 1) * iron / target_amount) as int
+	else:
+		VictoryHandler.you_win.emit()
+		portal_sprite.frame = frame_count - 1
