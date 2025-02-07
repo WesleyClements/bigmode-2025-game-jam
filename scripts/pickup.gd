30c7d9d66e91d4ea547846f88f151fa9c47c9140
WesleyClements <wesley.clements@gmail.com>
2025-02-06 19:00:28 -0500

    made mining lasers suck slower and smoothly

--- scripts/pickup.gd
+++ scripts/pickup.gd
@@ -5,18 +5,23 @@ const ItemType = MessageBuss.ItemType
 
 @export var item_type: ItemType
 @export var amount: int = 1
-@export var speed: float = 70.0
-@export var close_enough_distance: float = 5.0
+@export var suck_speed: float = 4.0
+@export var suck_speed_curve: Curve
+@export var player_suck_speed: float = 70.0
+@export var curve_max_distance: float = 48.0
 
 var target: Node2D: set = set_target
 
 func _physics_process(delta):
 	if target == null:
 		return
+	var current_distance := global_position.distance_to(target.global_position)
+	var speed: float
+	if target.is_in_group(&"player"):
+		speed = player_suck_speed
+	else:
+		speed = suck_speed * suck_speed_curve.sample(minf(current_distance / curve_max_distance, 1.0))
 	global_position = global_position.move_toward(target.global_position, delta * speed)
-	if global_position.distance_to(target.global_position) > close_enough_distance:
-		return
-	target = null
 
 func get_type() -> ItemType:
 	return item_type
