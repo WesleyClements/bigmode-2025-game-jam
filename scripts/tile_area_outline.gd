f869bbe7297e4b818815f06e14488cf4a169b076
Wesley Clements <wesley.clements@gmail.com>
2025-02-10 13:54:33 -0500

    filled in tile area outline to make visuals more clear

--- scripts/tile_area_outline.gd
+++ scripts/tile_area_outline.gd
@@ -8,12 +8,24 @@ const WorldTileMapLayer = preload("res://scripts/WorldTileMapLayer.gd")
 	get:
 		return root_node if root_node else owner
 @export var tile_radius := 4
-
-var _editor_points: PackedVector2Array = []
+@export var fill := false:
+	set(value):
+		if fill == value:
+			return
+		fill = value
+		queue_redraw()
 
 func _draw():
+	if points.size() == 0:
+		return
+	var poly_points: PackedVector2Array = points
+	poly_points.append(points[0])
+	if fill:
+		var poly_color := default_color
+		poly_color.a *= 0.5
+		draw_colored_polygon(poly_points, poly_color)
 	if Engine.is_editor_hint():
-		draw_polyline(_editor_points, default_color, width)
+		draw_polyline(poly_points, default_color, width)
 
 func _ready():
 	var tile_size := get_tile_size()
@@ -26,12 +38,9 @@ func _ready():
 		Vector2(0, -y),
 		Vector2(-x, 0),
 	]
-	if Engine.is_editor_hint():
-		_editor_points = outline_points
-		_editor_points.append(outline_points[0])
-		queue_redraw()
-	else:
+	if not Engine.is_editor_hint():
 		points = outline_points
+	queue_redraw()
 
 
 func get_tile_size() -> Vector2:
